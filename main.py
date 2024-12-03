33adba54bd0f7047b357ab215ec1c67593ecca83
Idmona <tanya.nemchenko23.85@gmail.com>
2024-12-03 23:46:06 +0300

    new commit

---
+++ main.py
@@ -0,0 +1,89 @@
+import os
+import file_operations
+from faker import Faker
+import random
+
+
+SKILLS = [
+    'Стремительный прыжок',
+    'Электрический выстрел',
+    'Ледяной удар',
+    'Кислотный взгляд',
+    'Тайный побег',
+    'Огненный заряд',
+]
+
+LETTERS = {
+    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+    ' ': ' '
+}
+
+
+OUTPUT_FOLDER = "cards"
+NUM_CARDS = 10
+
+
+def to_symbols(text):
+    for char, replacement in LETTERS.items():
+        text = text.replace(char, replacement)
+    return text
+
+
+def generate_context(fake, skills):
+    runic_skills = [to_symbols(skill) for skill in random.sample(skills, 3)]
+    return {
+        "first_name": fake.first_name(),
+        "last_name": fake.last_name(),
+        "job": fake.job(),
+        "town": fake.city(),
+        "strength": random.randint(3, 18),
+        "agility": random.randint(3, 18),
+        "endurance": random.randint(3, 18),
+        "intelligence": random.randint(3, 18),
+        "luck": random.randint(3, 18),
+        "skill_1": runic_skills[0],
+        "skill_2": runic_skills[1],
+        "skill_3": runic_skills[2],
+    }
+
+
+def save_card(fake, skills, card_number):
+    context = generate_context(fake, skills)
+    output_filename = os.path.join(
+        OUTPUT_FOLDER, f"charsheet_result_{card_number}.svg")
+    file_operations.render_template("charsheet.svg", output_filename, context)
+
+
+def main():
+    if not os.path.exists(OUTPUT_FOLDER):
+        os.makedirs(OUTPUT_FOLDER)
+
+    fake = Faker("ru_RU")
+
+    for i in range(1, NUM_CARDS + 1):
+        save_card(fake, SKILLS, i)
+
+
+if __name__ == "__main__":
+    main()
